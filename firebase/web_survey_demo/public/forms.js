75b2549b5407f7dd8f120fd9ea4de5b77f75586f
akioda <akioda@localhost.localdomain>
2021-08-16 15:20:57 +0900

    [forms.js] hide each component

--- firebase/web_survey_demo/public/forms.js
+++ firebase/web_survey_demo/public/forms.js
@@ -1,7 +1,7 @@
 export default {
   data: function() {
 	 return{
-		 survey_name:'',
+		 survey_data:[],
 		 yes_or_no:true,
 		 level:'3',
 		 opinion_text:''
@@ -12,7 +12,7 @@ export default {
 		var self = this;
 		var event = "";
 
-		var event_ref = firebase.firestore().collection("survey_data").doc(self.survey_name).collection("opinion");
+		var event_ref = firebase.firestore().collection("survey_data").doc(self.survey_data.name).collection("opinion");
 		event_ref.add({
 			opinion: self.opinion_text,
 			timestamp: firebase.firestore.Timestamp.fromDate(new Date())
@@ -26,7 +26,7 @@ export default {
 	push_yes_or_no(bool_data){
 		var self = this;
 
-		var event_ref = firebase.firestore().collection("survey_data").doc(self.survey_name).collection("yes_or_no");
+		var event_ref = firebase.firestore().collection("survey_data").doc(self.survey_data.name).collection("yes_or_no");
 		event_ref.add({
 				answer: bool_data,
 				timestamp: firebase.firestore.Timestamp.fromDate(new Date())
@@ -39,7 +39,7 @@ export default {
 	push_level(){
 		var self = this;
 
-		var event_ref = firebase.firestore().collection("survey_data").doc(self.survey_name).collection("level");
+		var event_ref = firebase.firestore().collection("survey_data").doc(self.survey_data.name).collection("level");
 		event_ref.add({
 				answer: self.level,
 				timestamp: firebase.firestore.Timestamp.fromDate(new Date())
@@ -52,32 +52,31 @@ export default {
   },
   mounted: function(){
 	var self = this;
-	
-	var elems1 = document.querySelectorAll('.collapsible');
-	var instances1 = M.Collapsible.init(elems1);
 
 	firebase.firestore().collection("summary").doc("current_survey").onSnapshot((doc) => {
-		self.survey_name = doc.data().name;
+		self.survey_data = doc.data();
 	});
   },
   template:`
 		<div> 
                   <div class="row">
 	            <div class="col s12">
-		      <blockquote><pre>{{ survey_name }}</pre></blockquote>
+		      <blockquote><pre>{{ survey_data.name }}</pre></blockquote>
 		    </div>
-		    <div class="card blue lighten-4 col s5 btn waves-effect waves-light" v-on:click="push_yes_or_no(true)">
-	              <div class="col s12 center-align">
-		        YES
-	              </div>
+		    <div class="row" v-if="survey_data.yes_or_no">
+		      <div class="card blue lighten-4 col s5 btn waves-effect waves-light" v-on:click="push_yes_or_no(true)">
+	                <div class="col s12 center-align">
+		          YES
+	                </div>
+		      </div>
+		      <div class="col s2"></div>
+		      <div class="card red lighten-4 col s5 btn waves-effect waves-light" v-on:click="push_yes_or_no(false)">
+	                <div class="col s12 center-align">
+		          NO
+	                </div>
+		      </div>
 		    </div>
-		    <div class="col s2"></div>
-		    <div class="card red lighten-4 col s5 btn waves-effect waves-light" v-on:click="push_yes_or_no(false)">
-	              <div class="col s12 center-align">
-		        NO
-	              </div>
-		    </div>
-		    <div class="row">
+		    <div class="row" v-if="survey_data.level">
 		      <form action="#" class="col s9">
 		        <p class="range-field">
 		          <input type="range" id="test5" v-model="level"  min="1" max="5" />
@@ -85,7 +84,7 @@ export default {
 	              </form>
 	              <div class="col s3  btn waves-effect waves-light blue" v-on:click="push_level">決定:{{ level }}</div>
 		    </div>
-		    <div class="row valign-wrapper">
+		    <div class="row valign-wrapper" v-if="survey_data.opinion">
 		      <div class="input-field col s11">
 		        <i class="material-icons prefix">mode_edit</i>
 		        <textarea id="textarea1" class="materialize-textarea" v-model="opinion_text" ></textarea>
